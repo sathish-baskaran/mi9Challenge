157927f0ee4aa9cbc2f6bb9a7af5d94757384501
SATHISH BASKARAN <sathishadelaide@yahoo.com.au>
2015-02-19 01:38:03 +0000

    files

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,12 +2,21 @@
     
     var data = require("../data");
     
+    var jq = require('jquery');
+    
     homeController.init = function (app) {
         
         //SB: Call and load data from node server, this is same as calling web service from .net framework
         app.get('/', function (req, res) {
+        
             
             data.getPayload(function (err, results) {
+                
+                 var asData = jq(results).filter(function (i, n) { return n.drm == true && n.episodeCount > 0 });
+                
+                 // res.setHeader('Content-Type', 'application/json');
+                 res.json(JSON.stringify(asData));
+                  
                 res.render("index" , { title : 'Mi9.Coding', error : err, loads : results });
             });
 
--- server.js
+++ server.js
@@ -4,23 +4,38 @@
 
 var http = require('http');
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var express = require('express');
 var app = express();
 
-var controllers = require("./controllers");
+//var controllers = require("./controllers");
 
 //SB: assign vash engine for view
 app.set("view engine", "vash");
 
 
 //SB: call homecontroller
-controllers.init(app);
+//controllers.init(app);
 
-app.use('/*', function (req, res, next) {
-    res.render( "../views/error_template" , { title : 'Sorry, Page not found', error : 'Sorry, page not found' });
+var data = require("./data");
+var jq = require('jquery');
+  
+  
+
+app.get('/', function (req, res) {
+
+    data.getPayload(function (err, results) {
+        
+    var asData = jq(results).filter(function (i, n) { return n.drm == true && n.episodeCount > 0 });
+        
+      res.render("index" , { title : 'Mi9.Coding', error : err, loads : JSON.stringify(asData) });
+
+
+    });
+
+   
 });
 
 //SB: create server instance to handle from client side
